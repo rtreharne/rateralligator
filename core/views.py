84bfc16ff38de4cc7b15cbb75271ac58ca143797
JackJFoster <jack.foster1878@gmail.com>
2023-06-29 11:51:57 +0100

    new version

--- core/views.py
+++ core/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import NewAlligatorForm
 
 def index(request):
-    return HttpResponse("Hello, world")
+    return render(request, "core/index.html")
+
+def create(request):
+    form = NewAlligatorForm()
+
+    return render(request, "core/create.html", {"form": form})
+
